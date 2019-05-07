196b2e0f9a1ebc4a4f1a2ad26bb55c2aedaf2633
Kashif1Naqvi <naqvishah582@gmail.com>
2019-05-07 17:55:15 +0500

    operators in javascript numbers

---
+++ Strings/operators.js
@@ -0,0 +1,28 @@
+
+// operator is so much imporatnt in programming languages for performing  arthmaticle and logical operations
+
+
+function operator(a,b){
+    function add(a,b){
+        console.log(`${a} + ${b} = ${a+b}`)
+    }
+    function div(a,b){
+        console.log(`${a} / ${b} = ${a/b}`)
+    }
+    function mul(a,b){
+        console.log(`${a} * ${b} = ${a*b}`)
+    }
+    function sub(a,b){
+        console.log(`${a} - ${b} = ${a-b}`)
+    }
+    function mod(a,b){
+        console.log(`${a} % ${b} = ${a % b}`)
+    }
+    add(2,2)
+    div(10,2)
+    mul(3,3)
+    sub(10,7)
+    mod(10,5)
+    console.log((a+b)*a)
+}
+operator(2,2)
