a471552ec7a5156ae73e09eadbf6a195d0b3c191
Kashif1Naqvi <naqvishah582@gmail.com>
2019-05-07 09:00:41 +0500

    nameing variables

---
+++ variable/myscript.js
@@ -0,0 +1,26 @@
+var myDiv = document.getElementById('myDiv');
+
+myDiv.style.background = 'red';
+myDiv.style.color      = 'white';
+myDiv.style.margin = '50px';
+
+// valide variables name
+
+var name = "kashif naqvi"
+var Name = "Syed kashif naqvi"
+var _name= "kashif naqvi";
+var p123 = "BSCS";
+var $important = "what is this?"
+
+
+//invalide variable name
+
+
+var #name = "kashif naqvi"
+var %code = "code is art";
+var ^error = "this is error"
+var name-last ="naqvi";
+var ~codeing = "if you should do very depply then you learn a lot"
+var 1Program ="BSCS"
+
+
