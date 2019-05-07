8ce7066bc2cb4bc5f834823b7ffc54c1239dad3d
Kashif1Naqvi <naqvishah582@gmail.com>
2019-05-07 14:39:12 +0500

    strings

---
+++ Strings/string.js
@@ -0,0 +1,24 @@
+// string is the collect of CharacterData,that are use to mangae diffrent component into the words
+
+// so in very simple examples you understand the Prorgram
+
+
+var myString = "hello how are you man.Lets enjoy with code :) "
+console.log(myString)
+
+// if i use multiline string the show me identfier error but a one key word /n i can write then it's working
+
+var multilineString = "1 this is the first line  \n 2 this is the second line \n3 this is the third line \n"
+console.log(multilineString);
+
+var complex_string = 'sir can saying me, \"student please learn from code.your\'s mood is not for doing programming (: \"';
+var qutes_ALias    ='IMAM ALI AS CAN SAY,\'A WISE MEN CAN SAY SOMETHING,BUT A FOOL ALWAYS SAY SOMETHING\'' ;
+
+console.log(qutes_ALias);
+
+console.log(complex_string)
+
+// concatination is merge two strings or numbers etc
+// \t use for create spacing
+var a = "hello"
+console.log(a + "\tworld" );
